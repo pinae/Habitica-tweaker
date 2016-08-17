8a77498fb71561816e1e0679f28e14a600d08f5a
pinae <jonny@pinae.net>
2016-08-17 09:37:09 +0200

    started habitipy

---
+++ habitipy.py
@@ -0,0 +1,146 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+import requests, json
+
+
+class HabiticaAccount:
+    def __init__(self, user_id, api_key):
+        self.headers = {'x-api-user': user_id, 'x-api-key': api_key}
+
+    def get_tags(self):
+        response = requests.get('https://habitica.com/api/v3/tags', headers=self.headers)
+        return response.json()['data']
+
+    def get_tag_id(self, tag_name):
+        for tag in self.get_tags():
+            if tag['name'] == tag_name:
+                return tag['id']
+
+    def get_tasks(self):
+        response = requests.get('https://habitica.com/api/v3/tasks/user', headers=self.headers)
+        return response.json()['data']
+
+    def get_tasks_with_tag(self, tag_id):
+        task_list = []
+        for task in self.get_tasks():
+            if tag_id in task['tags']:
+                task_list.append(task)
+        return task_list
+
+    def create_daily(self,
+                     text,
+                     repeat_days=False,
+                     every_x=False,
+                     checklist=False,
+                     notes='',
+                     priority=1,
+                     attribute='str',
+                     value=1.947771358054911):
+        if priority not in [0.1, 1, 1.5, 2]:
+            priority = 1
+        new_task = {
+            'text': text,
+            'notes': notes,
+            'priority': priority,
+            'attribute': attribute,
+            'value': value,
+            'type': 'daily'
+        }
+        if checklist:
+            new_task['checklist'] = checklist
+        if repeat_days:
+            new_task['everyX'] = 1
+            new_task['repeat'] = repeat_days
+            new_task['frequency'] = 'weekly'
+        elif every_x:
+            new_task['everyX'] = every_x
+            new_task['repeat'] = {'m': True, 't': True, 'w': True, 'th': True, 'f': True,
+                                  's': True, 'su': True}
+            new_task['frequency'] = 'daily'
+        response = requests.post('https://habitica.com/api/v3/tasks/user',
+                                 json=[new_task],
+                                 headers=self.headers)
+        if response.status_code != 201:
+            print(response.json())
+        return response.status_code == 201
+
+    def create_habit(self,
+                     text,
+                     good=True,
+                     bad=False,
+                     notes='',
+                     priority=1,
+                     attribute='str',
+                     value=1.947771358054911):
+        if not good and not bad:
+            good = True
+        if priority not in [0.1, 1, 1.5, 2]:
+            priority = 1
+        new_task = {
+            'text': text,
+            'up': good,
+            'down': bad,
+            'notes': notes,
+            'priority': priority,
+            'attribute': attribute,
+            'value': value,
+            'type': 'habit'
+        }
+        response = requests.post('https://habitica.com/api/v3/tasks/user',
+                                 json=[new_task],
+                                 headers=self.headers)
+        if response.status_code != 201:
+            print(response.json())
+        return response.status_code == 201
+
+    def create_todo(self,
+                    text,
+                    notes='',
+                    priority=1,
+                    checklist=False,
+                    attribute='str',
+                    value=False,
+                    completed=False):
+        if priority not in [0.1, 1, 1.5, 2]:
+            priority = 1
+        new_task = {
+            'text': text,
+            'notes': notes,
+            'priority': priority,
+            'attribute': attribute,
+            'completed': completed,
+            'type': 'todo'
+        }
+        if checklist:
+            new_task['checklist'] = checklist
+        if value:
+            new_task['value'] = value
+        response = requests.post('https://habitica.com/api/v3/tasks/user',
+                                 json=[new_task],
+                                 headers=self.headers)
+        if response.status_code != 201:
+            print(response.json())
+        return response.status_code == 201
+
+    def get_stats(self):
+        response = requests.get('https://habitica.com/api/v3/user', headers=self.headers)
+        return response.json()['data']['stats']
+
+    def get_user_data(self):
+        response = requests.get('https://habitica.com/api/v3/user', headers=self.headers)
+        return response.json()['data']
+
+    def get_current_quest(self):
+        response = requests.get('https://habitica.com/api/v3/user', headers=self.headers)
+        return response.json()['data']['party']['quest']
+
+    def get_group_id(self):
+        response = requests.get('https://habitica.com/api/v3/groups/party', headers=self.headers)
+        return response.json()['data']['id']
+
+    def post_message_to_party(self, message):
+        response = requests.post('https://habitica.com/api/v3/groups/party/chat',
+                                 json={"message": message},
+                                 headers=self.headers)
+        return response.status_code == 200
---
+++ tweaker.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import division, print_function, unicode_literals
+import habitipy
+import requests
+try:
+    from private_settings import accounts
+except ImportError:
+    accounts = []
+
+
+if __name__ == "__main__":
+    print("Hallo Welt.")
+    """headers = {'x-api-user': USER_ID, 'x-api-key': API_KEY}
+    response = requests.get('https://habitica.com/api/v3/groups/party', headers=headers)
+    group_id = response.json()['data']['id']
+    print(group_id)
+    response = requests.get('https://habitica.com/api/v3/user', headers=headers)
+    last_cron = response.json()['data']['lastCron']  # "2016-08-16T06:37:22.388Z"
+    mana = response.json()['data']['stats']['mp']
+    health = response.json()['data']['stats']['hp']
+    print(mana)
+    print(health)"""
+    import json
+    #print(json.dumps(response.json(), sort_keys=True, indent=4))
+    main_account = habitipy.HabiticaAccount(accounts[0][0]['USER_ID'], accounts[0][0]['API_KEY'])
+    print(json.dumps(main_account.get_tasks(), sort_keys=True, indent=4))
+    #print(json.dumps(main_account.get_current_quest(), sort_keys=True, indent=4))
+    work_account = habitipy.HabiticaAccount(accounts[0][1]['USER_ID'], accounts[0][1]['API_KEY'])
+    #print(json.dumps(work_account.get_current_quest(), sort_keys=True, indent=4))
+    #print(not work_account.get_current_quest()['key'])
+    print(work_account.create_daily("test 1", repeat_days={'m': 0, 't': 1, 'w': 0, 'th': 1, 'f': 0, 's': 0, 'su': 0},
+                                    priority=0.1))
+    print(work_account.create_daily("test 2", every_x=3, priority=1.5))
+    print(work_account.create_habit("some habit", priority=2))
+    print(work_account.create_habit("bad habit", good=False, bad=True))
+    print(work_account.create_todo("some todo", priority=1.5))
+
