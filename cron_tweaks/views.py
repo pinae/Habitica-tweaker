8554a7b6cbd08f639180574192ed04d8cf64b5d6
pinae <jonny@pinae.net>
2016-08-19 11:22:42 +0200

    Support for "Fuck my life!"

--- cron_tweaks/views.py
+++ cron_tweaks/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def fuck_my_life(account):
+    if not account.habitipy().get_current_quest()['key']:
+        tasks = account.habitipy().get_tasks()
+        for task in tasks:
+            if task['text'] == "Fuck my life!":
+                try:
+                    target = int(task['notes'])
+                    while account.habitipy().get_stats()['hp'] > target:
+                        account.habitipy().score_task(task['id'], down=True)
+                    break
+                except ValueError:
+                    break
